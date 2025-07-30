f90eb1756742a7fd7cecde7211576fa8785eecd0
Nick Whitelegg <nickw4426@gmail.com>
2025-07-30 16:00:26 +0100

    Initial commit

---
+++ src/Cat.kt
@@ -0,0 +1,17 @@
+class Cat (val name: String, var age: Int, var weight: Int) {
+    // Make the cat walk.
+    // Reduces weight by one
+    // Should check if the weight is 5 or more
+    // There is a bug - don't resolve until instructed by the exercise.
+    fun walk() :Boolean {
+        if(weight <= 5) {
+            weight--
+            return true
+        }
+        return false
+    }
+
+    override fun toString() : String {
+        return "Name: $name Age: $age Weight: $weight"
+    }
+}
